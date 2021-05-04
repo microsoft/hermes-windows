d0b97f2fe660740423b8d2e19d2787a41209a530
Anandraj Govindan <anandrag@microsoft.com>
2021-05-04 02:19:40 -0700

    Building hermes inspector dll

---
+++ .ado/scripts/prepare_boost.ps1
@@ -0,0 +1,15 @@
+[cmdletbinding(SupportsShouldProcess=$True)]
+param(
+    [Parameter(Mandatory=$False)]
+    [string]$BOOST_VERSION="1_63_0",
+
+    [Parameter(Mandatory=$False)]
+    [string]$DOWNLOADDIR="downloads",
+
+    [Parameter(Mandatory=$False)]
+    [string]$BOOST_EXTRACTED="boost_src"
+)
+
+$NugetURL = 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe'
+Invoke-WebRequest -Uri $NugetURL -OutFile "nuget.exe"
+Invoke-Expression ".\nuget.exe install boost -Version 1.72.0"
