581eaf8fa512b05273639e9aa7ad54b7d37b4ff8
Anandraj Govindan <anandrag@microsoft.com>
2021-05-04 03:59:38 -0700

    Boost fix

--- .ado/scripts/prepare_boost.ps1
+++ .ado/scripts/prepare_boost.ps1
@@ -13,4 +13,4 @@ param(
 $NugetURL = 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe'
 Invoke-WebRequest -Uri $NugetURL -OutFile "nuget.exe"
 Invoke-Expression ".\nuget.exe sources Enable -Name nuget.org"
-Invoke-Expression ".\nuget.exe install boost -Version 1.72.0"
+Invoke-Expression ".\nuget.exe install boost -Version 1.72.0 -Config $PSScriptRoot\Nuget.Config"
