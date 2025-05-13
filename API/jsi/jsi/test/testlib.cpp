ee9e66736180dac9860ebe23c20d9814f669e6ee
Vineeth <66076509+vineethkuttan@users.noreply.github.com>
2025-05-13 20:31:15 +0530

    Update testlib.cpp

--- API/jsi/jsi/test/testlib.cpp
+++ API/jsi/jsi/test/testlib.cpp
@@ -1085,7 +1085,7 @@ TEST_P(JSITest, JSErrorStackOverflowHandling) {
     eval("(function f() { callSomething(); f.apply(); })()");
     FAIL();
   } catch (const JSError& ex) {
-    EXPECT_NE(std::string(ex.what()).c_str(), "exceeded");
+    EXPECT_EQ(std::string(ex.what()).c_str(), "exceeded");
   }
 }
 
